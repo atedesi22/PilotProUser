ee855db3a2713ac1fb21434600b1ec2645a45010
atedesi22 <paulemmanuelbohole5@gmail.com>
2025-11-15 00:10:37 +0100

    new commit

--- app/Models/Tenant/Utilisateur/Utilisateur.php
+++ app/Models/Tenant/Utilisateur/Utilisateur.php
@@ -3,8 +3,29 @@
 namespace App\Models\Tenant\Utilisateur;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Utilisateur extends Model
+class Utilisateur extends Authenticatable
 {
     //
+    protected $connection = 'tenant';
+    protected $table = 'utilisateurs';
+    protected $primaryKey = 'id_utilisateur';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    
+    protected $fillable = ['id_utilisateur', 'nom_complet', 'email', 'mot_de_passe', 'id_role'];
+    protected $hidden = ['mot_de_passe'];
+
+    // RELATION : Un utilisateur a un rôle
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'id_role', 'id_role');
+    }
+    
+    // RELATION : Un utilisateur peut être un employé
+    public function employe()
+    {
+        return $this->hasOne(Employe::class, 'id_utilisateur', 'id_utilisateur');
+    }
 }
